35d62a8e9d2399c0830f9fb1ae8f38c3e5a4f864
Pushpender Singh <52063297+pushpinder6805@users.noreply.github.com>
2025-08-10 19:01:53 +0530

    +

--- javascripts/discourse/initializers/init-topic-excerpts.js
+++
@@ -1,8 +0,0 @@
-import { apiInitializer } from "discourse/lib/api";
-
-export default apiInitializer("0.8", (api) => {
-  api.modifyClass("component:topic-list-item", {
-    pluginId: "discourse-air",
-    expandPinned: true,
-  });
-});
